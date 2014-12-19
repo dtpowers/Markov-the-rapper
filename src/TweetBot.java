e2a3f95a07188aacf13dfb1945fe9c25cfbfb8e3
Donovan Powers <dontpowers@hotmail.com>
2014-12-19 04:02:36 -0500

    fixed null pointer exception by ensuring tweet only continued if map had words left

--- src/TweetBot.java
+++ src/TweetBot.java
@@ -10,6 +10,7 @@ public class TweetBot {
 	public TweetBot(String sampleText) throws IOException {
 		wordMap = Markov.genWordTable(sampleText);
 		String tweetTxt = generateTweetTxt();
+		System.out.print(tweetTxt);
 	}
 	
 	public static String getRandomWord(HashMap<String, ArrayList<String>> wordMap){
@@ -32,19 +33,23 @@ public class TweetBot {
 		//current word, used to get next word.
 		String currWord = tweet;
 		String nextWord;
-		int tweetLen = 0;
+		tweet += " ";
+		int tweetLen = tweet.length();
 		Random randGen = new Random();
 		boolean isDone = false;
 		//until tweet is max len keep picking words
-		while(isDone){
-			int rand = randGen.nextInt(wordMap.get(currWord).size());
+		
+		while(!isDone && wordMap.get(currWord) != null){
+			
+			int listSize = wordMap.get(currWord).size();
+			int rand = randGen.nextInt(listSize);
 			nextWord = wordMap.get(currWord).get(rand);
 			tweetLen += nextWord.length();
 			if(tweetLen > 140){
 				isDone = true;
 			}
 			else{
-				tweet += nextWord;
+				tweet += nextWord + " ";
 				currWord = nextWord;
 			}
 			
