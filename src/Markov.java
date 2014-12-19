7d4322081b9ffe664a930a286cb5023cf6ada8f4
Donovan Powers <dontpowers@hotmail.com>
2014-12-19 04:07:49 -0500

    added end of line concept to markov chain

--- src/Markov.java
+++ src/Markov.java
@@ -7,50 +7,48 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
-
-
 public class Markov {
-	
-	//create hash table for every word that contains each word that follows it
-	//piece together a sentence
-	public static HashMap<String, ArrayList<String>> genWordTable(String sourceDest) throws IOException{
+
+	// create hash table for every word that contains each word that follows it
+	// piece together a sentence
+	public static HashMap<String, ArrayList<String>> genWordTable(
+			String sourceDest) throws IOException {
 		File sourceText = new File(sourceDest);
 		BufferedReader br = new BufferedReader(new FileReader(sourceText));
 		HashMap<String, ArrayList<String>> result = new HashMap<String, ArrayList<String>>();
-		//words are separated by a space
-		//for each word in file put word in map with key value of next word
+		// words are separated by a space
+		// for each word in file put word in map with key value of next word
 		String line = br.readLine();
-		
-		while(line != null){
+
+		while (line != null) {
 			String[] words = line.split(" ");
-			//create list of words
-			
-			for(int i = 0; i<words.length - 1; i++){
-				//if the word is already in the map, add next word to list
-				if (result.containsKey(words[i])){
+			// create list of words
+
+			for (int i = 0; i < words.length - 1; i++) {
+				// if the word is already in the map, add next word to list
+				if (result.containsKey(words[i])) {
 					ArrayList<String> curValues = result.get(words[i]);
-					curValues.add(TextFormater.formatText(words[i+1]));
-				
+					curValues.add(TextFormater.formatText(words[i + 1]));
+
 					result.put(TextFormater.formatText(words[i]), curValues);
-					
-				} else{
-					//otherwise create new entry for word
+
+				} else {
+					// otherwise create new entry for word
 					ArrayList<String> values = new ArrayList<String>();
-					values.add(TextFormater.formatText(words[i+1]));
+					values.add(TextFormater.formatText(words[i + 1]));
 					result.put(TextFormater.formatText(words[i]), values);
 				}
-				
+
 			}
-			
-			
-		
+			// handle last word of line
+			ArrayList<String> values = new ArrayList<String>();
+			result.put(TextFormater.formatText(words[words.length]), values);
+
 			line = br.readLine();
 		}
-		
-		
+
 		br.close();
 		return result;
 	}
-	
 
 }
