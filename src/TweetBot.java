c47f3fc7521ed829973bd2e4176f41323b4ae81d
Donovan Powers <dontpowers@hotmail.com>
2014-12-19 20:43:16 -0500

    created run function, server structure

--- src/TweetBot.java
+++ src/TweetBot.java
@@ -10,26 +10,36 @@ import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-
 public class TweetBot {
 	public static HashMap<String, ArrayList<String>> wordMap;
-	public TweetBot(String sampleText) throws IOException {
+
+	public TweetBot(String sampleText) throws IOException, InterruptedException {
 		wordMap = Markov.genWordTable(sampleText);
-		String tweetTxt = generateTweetTxt();
 		Twitter twitterBot = setup();
+		run(twitterBot);
+		
+
+	}
+
+	public static void run(Twitter bot) throws InterruptedException {
+		while (true) {
+			followBack(bot);
+			postTweet(bot);
+			Thread.sleep(1000 * 60 * 15);
+		}
+	}
+
+	public static void followBack(Twitter bot) {
+
+	}
+
+	public static void postTweet(Twitter bot) {
+		String tweetTxt = generateTweetTxt();
 		System.out.print(tweetTxt);
-		followBack(twitterBot);
-		postTweet(twitterBot);
-		
 	}
-	public static void followBack(Twitter bot){
-		
-	}
-	public static void postTweet(Twitter bot){
-		
-	}
-	public static Twitter setup() throws IOException{
-		//configure bot settings
+
+	public static Twitter setup() throws IOException {
+		// configure bot settings
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setOAuthConsumerKey(getConsumerKey("consumerKey.txt"));
 		cb.setOAuthConsumerSecret(getConsumerKeySecret("consumerSec.txt"));
@@ -39,26 +49,31 @@ public class TweetBot {
 		Twitter twitter = tf.getInstance();
 		return twitter;
 	}
-	public static String getConsumerKey(String dir) throws IOException{
+
+	public static String getConsumerKey(String dir) throws IOException {
 		File key = new File(dir);
 		String result = deserializeString(key);
 		return result;
 	}
-	public static String getConsumerKeySecret(String dir) throws IOException{
+
+	public static String getConsumerKeySecret(String dir) throws IOException {
 		File key = new File(dir);
 		String result = deserializeString(key);
 		return result;
 	}
-	public static String getToken(String dir) throws IOException{
+
+	public static String getToken(String dir) throws IOException {
 		File key = new File(dir);
 		String result = deserializeString(key);
 		return result;
 	}
-	public static String getTokenSec(String dir) throws IOException{
+
+	public static String getTokenSec(String dir) throws IOException {
 		File key = new File(dir);
 		String result = deserializeString(key);
 		return result;
 	}
+
 	public static String deserializeString(File file) throws IOException {
 		int len;
 		char[] chr = new char[4096];
@@ -73,46 +88,49 @@ public class TweetBot {
 		}
 		return buffer.toString();
 	}
-	public static String getRandomWord(HashMap<String, ArrayList<String>> wordMap){
+
+	public static String getRandomWord(
+			HashMap<String, ArrayList<String>> wordMap) {
 		Set<String> words = wordMap.keySet();
 		int size = words.size();
-		int item = new Random().nextInt(size); // In real life, the Random object should be rather more shared than this
+		int item = new Random().nextInt(size); // In real life, the Random
+												// object should be rather more
+												// shared than this
 		int i = 0;
-		for(Object obj : words)
-		{
-		    if (i == item)
-		        return obj.toString();
-		    i = i + 1;
+		for (Object obj : words) {
+			if (i == item)
+				return obj.toString();
+			i = i + 1;
 		}
 		return null;
-	
+
 	}
-	public static String generateTweetTxt(){
-		//start by randomly selecting a first word
+
+	public static String generateTweetTxt() {
+		// start by randomly selecting a first word
 		String tweet = getRandomWord(wordMap);
-		//current word, used to get next word.
+		// current word, used to get next word.
 		String currWord = tweet;
 		String nextWord;
 		tweet += " ";
 		int tweetLen = tweet.length();
 		Random randGen = new Random();
 		boolean isDone = false;
-		//until tweet is max len keep picking words
-		
-		while(tweet.length() < 140 && !isDone && wordMap.get(currWord) != null){
-			
+		// until tweet is max len keep picking words
+
+		while (tweet.length() < 140 && !isDone && wordMap.get(currWord) != null) {
+
 			int listSize = wordMap.get(currWord).size();
 			int rand = randGen.nextInt(listSize);
 			nextWord = wordMap.get(currWord).get(rand);
 			tweetLen += nextWord.length();
-			if(tweetLen > 140){
+			if (tweetLen > 140) {
 				isDone = true;
-			}
-			else{
+			} else {
 				tweet += nextWord + " ";
 				currWord = nextWord;
 			}
-			
+
 		}
 		return tweet;
 	}
